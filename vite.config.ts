c67413cb363f7d746851ef0f76bad2df19044f5d
Suhaib28 <suhaib.siddiqui2003@gmail.com>
2026-09-17 17:18:19 -0700

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
+﻿import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: "/study-adapt/",
+  plugins: [react()],
   server: {
     host: "::",
     port: 8080,
@@ -16,4 +17,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
